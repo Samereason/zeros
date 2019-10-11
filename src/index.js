e827e09954ebabcaedd473410360dc71e1316c01
Yurii Shem <asreil1994@gmail.com>
2019-10-11 22:04:29 +0300

    first draft

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function zeros(expression) {
   // your solution
+};
+
+const exp = '45!*63!*28!';
+
+function divFive(int) {
+	let result = 0;
+
+	while (int % 5 === 0 ) {
+		int = int / 5;
+		result++;
+	}
+
+	return result;
 }
+
+function factZero (fact) {
+	let x = 0;
+
+	for (let i = 5; i <= fact; i+=5) {
+		x+= divFive(i);
+	}
+
+	return x;
+}
+
+const arr = exp.split("*").map(item => factZero(+item.slice(0, -1)));
+
+const result = arr.reduce((sum, current) => sum + current);
+
+console.log(result);
