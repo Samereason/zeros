d8df096b828f5f95223698df8da912adea47e063
Yurii Shem <asreil1994@gmail.com>
2019-10-12 13:55:24 +0300

    fail two tests *(

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
 module.exports = function zeros(expression) {
   // your solution
+
+	function divFive(int) {
+		let result = 0;
+
+		while (int % 5 === 0 ) {
+			int = int / 5;
+			result++;
+		}
+
+		return result;
+	}
+
+	function factZero (fact, start, step) {
+		let x = 0;
+
+		for (let i = start; i <= fact; i+=step) {
+			x+= divFive(i);
+		}
+
+		return x;
+	}
+
+	const arr = expression.split("*").map(item => {
+		const fact = parseInt(item);
+		const factType = (item.match(/\!/g)||[]).length
+
+		if (factType > 1) {
+			return (fact % 2 === 0) ? factZero(fact, 2, 2) : factZero(fact, 1, 2)
+		} else {
+			return factZero(fact, 5, 5);
+		}
+	});
+
+	 return arr.reduce((sum, current) => sum + current);
 };
 
-const exp = '45!*63!*28!';
-
-function divFive(int) {
-	let result = 0;
-
-	while (int % 5 === 0 ) {
-		int = int / 5;
-		result++;
-	}
-
-	return result;
-}
-
-function factZero (fact) {
-	let x = 0;
-
-	for (let i = 5; i <= fact; i+=5) {
-		x+= divFive(i);
-	}
-
-	return x;
-}
-
-const arr = exp.split("*").map(item => factZero(+item.slice(0, -1)));
-
-const result = arr.reduce((sum, current) => sum + current);
-
-console.log(result);
